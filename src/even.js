fae86423f16a0d31b6faaf829e6fb89cd948240c
Rodion Zainullin <rodion-zainullin@mail.ru>
2021-10-16 20:27:20 +0700

    fix even.js

--- src/even.js
+++ src/even.js
@@ -1,26 +1,32 @@
 import readlineSync from 'readline-sync';
 
-const getRandomNumber = () => Math.floor(Math.random() * 50);
+function getRandomNumber() {
+  return Math.floor(Math.random() * 50);
+}
 const checkEven = (x) => x % 2 === 0;
-export default () => {
-  const name = readlineSync.question('May I have your name?');
-  console.log(`Hello, ${name}!`);
+const even = () => {
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
   let count = 0;
   do {
-    const random = getRandomNumber;
+    const random = getRandomNumber();
     const question = readlineSync.question(`Question: ${random}\n Your answer: `);
-    if (checkEven(random) && question === 'yes') {
+    if (checkEven(random) && question.toLowerCase() === 'yes') {
       console.log('Correct!');
       count += 1;
-    } else if (!checkEven(random) && question === 'no') {
+    } else if (!checkEven(random) && question.toLowerCase() === 'no') {
       console.log('Correct!');
       count += 1;
     } else {
+      console.log('your answer is wrong.');
+      console.log(`Let's try again, ${userName}`);
       break;
     }
   } while (count < 3);
   if (count === 3) {
-    console.log(`Congratulations, ${name}`);
+    console.log(`Congratulations, ${userName}`);
   }
 };
+
+export default even;
