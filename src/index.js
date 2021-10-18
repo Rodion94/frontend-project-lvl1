f730ae2237e0579cded0c0a0c2636bbb786e7bf9
Rodion Zainullin <rodion-zainullin@mail.ru>
2021-10-18 21:27:08 +0700

    add index.js

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import readlineSync from 'redline-sync';
+
+const newGame = (gameQuestion, gameQuestionAnswer) => {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}`);
+  console.log(`${gameQuestion}`);
+  for (let i = 0; i < 3; i += 1) {
+    const [question, answer] = gameQuestionAnswer();
+    console.log(`Question: ${question}`);
+    const yourAnswer = readlineSync.question('Your answer: ');
+    if (yourAnswer === answer) {
+      console.log('Correct');
+    } else {
+      return console.log(`${yourAnswer} is wrong answer ;(. Correct answer was ${answer}.\nLet's try again, ${name})`);
+    }
+  }
+  return console.log(`Congratulations, ${name}`);
+};
+
+export default newGame;
