65b6f646ed31f675b741055c2f177d06f92fcf7c
Rodion Zainullin <rodion-zainullin@mail.ru>
2021-10-22 21:01:04 +0700

    all updates

---
+++ bin/brain-prime.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+import brainPrime from "../src/games/game-prime.js";
+import newGame from "../src/index.js";
+
+brainPrime();
---
+++ src/games/game-prime.js
@@ -0,0 +1,26 @@
+import newGame from '../index.js';
+
+
+const isPrime = (num) => {
+  if (num < 2){
+    return false
+  }
+  for (let i = 2; i <= num / Math.sqrt(num); i += 1){
+    if (num % i === 0){
+      return false;
+    }
+  }
+  return true;
+};
+
+const gameQuestion = 'Answer "yes" if given number is prime. Otherwise answer "no"';
+const gameQuestionAnswer = () =>{
+  const maxNumForRandomNumber = 100;
+  const randomNumber = Math.floor(Math.random() * maxNumForRandomNumber);
+  const question = `${randomNumber}`;
+  const answer = isPrime(randomNumber) ? 'yes' : 'no';
+  return[question, answer];
+};
+const brainPrime = () => newGame(gameQuestion, gameQuestionAnswer);
+
+export default brainPrime;
