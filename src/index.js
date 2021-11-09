a145bca616b5afc34bc5c7c27996b427f36feba0
Rodion Zainullin <rodion-zainullin@mail.ru>
2021-11-09 16:38:18 +0700

    updates

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import readlineSync from 'readline-sync';
 
-
 const newGame = (gameQuestion, getGameQuestionAnswer) => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
